2cd43663295b2b97d782e90620b8eb29d1435ed4
Marcus Lagergren <1062473+lagergren@users.noreply.github.com>
2024-03-11 13:34:58 +0100

    Dev container (and prod container given artifacts) seems to be working.

--- docker/platform-build.sh
+++ docker/platform-build.sh
@@ -2,35 +2,38 @@
 
 echo "Ensuring the platform is built."
 
+export SRC_DIR=$PLATFORM_DIR/src
 
 function clone_platform() {
-  pushd $HOME/build
-  echo "Cloning to platform"
-  git clone --branch $GITHUB_BRANCH_PLATFORM --depth=1 https://github.com/xtclang/platform.git platform
-  echo "Writing last commit"
-  pushd platform
-  git rev-parse --verify HEAD > last_commit
-  popd
+  pushd $PLATFORM_DIR
+    echo "Cloning to platform"
+    git clone --branch $GITHUB_BRANCH_PLATFORM --depth=1 https://github.com/xtclang/platform.git src
+    echo "Writing last commit"
+    pushd $SRC_DIR
+      git rev-parse --verify HEAD > last_commit
+    popd
   popd
 }
 
 function check_updated_source() {
-  if [ -e $HOME/build/platform ]; then
+  echo "Checking for $SRC_DIR"
+  ls -lart $PLATFORM_DIR
+  if [ -e $SRC_DIR ]; then
     echo "Found existing source."
 
-    pushd $HOME/build/platform
-    _last_commit="unknown"
-    if [ -e last_commit ]; then
-      _last_commit=$(cat last_commit)
-    fi
-    _last_remote_commit=$(git rev-parse --verify HEAD)
+    pushd $SRC_DIR
+      _last_commit="unknown"
+      if [ -e last_commit ]; then
+        _last_commit=$(cat last_commit)
+      fi
+      _last_remote_commit=$(git ls-remote https://github.com/xtclang/platform.git platform-xtcplugin | awk '{ print $1 }')
     popd
 
     echo "last_commit       : $_last_commit"
     echo "last_remote_commit: $_last_remote_commit"
     if [ "$_last_commit" != "$_last_remote_commit" ]; then
-      echo "Existing source out of date."
-      rm -fr $HOME/build/platform
+      echo "Existing source out of date. REMOVING existing source dir: $SRC_DIR"
+      rm -fr $SRC_DIR
       clone_platform
     else
       echo "Existing platform is up date - good!"
@@ -43,7 +46,7 @@ function check_updated_source() {
 
 # Verify that the platform is built
 function build_platform() {
-  pushd $HOME/build/platform
-  ./gradlew build
+  pushd $SRC_DIR
+    ./gradlew build
   popd
 }

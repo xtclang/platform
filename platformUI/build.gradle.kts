6cc154fd342ff7c83c3671eace2dc8d5d6b0cfac
Gene Gleyzer <gene@xqiz.it>
2024-05-07 11:51:55 -0400

    Remove the old gui build

--- platformUI/build.gradle.kts
+++ platformUI/build.gradle.kts
@@ -20,10 +20,6 @@ tasks.register("build") {
 
     dependsOn(common.tasks["build"])
 
-    // there must be a way to tell quasar not to rebuild if nothing changed, but I cannot
-    // figure it out and have to use a manual timestamp check
-    dependsOn(checkGui)
-
     doLast {
         val srcModule = "${projectDir}/src/main/x/platformUI.x"
 
@@ -33,34 +29,6 @@ tasks.register("build") {
                         "-L", libDir,
                         "-r", webContent,
                         srcModule)
-        }
-    }
-}
-
-val checkGui = tasks.register("checkGui") {
-    group       = "Build"
-    description = "Build the web app content"
-
-    val src1 = fileTree("$projectDir/gui/src").files.stream().
-            mapToLong{f -> f.lastModified()}.max().orElse(0)
-    val src2 = fileTree("$projectDir/gui/public").files.stream().
-            mapToLong{f -> f.lastModified()}.max().orElse(0)
-    val dest = fileTree(webContent).files.stream().
-            mapToLong{f -> f.lastModified()}.max().orElse(0)
-
-    if (src1 > dest || src2 > dest) {
-        dependsOn(buildGui)
-        }
-    else {
-        println("$webContent is up to date")
-    }
-}
-
-val buildGui = tasks.register("buildGui") {
-    doLast {
-        project.exec {
-            workingDir(guiDir)
-            commandLine("yarn", "--ignore-engines", "quasar", "build")
         }
     }
 }
